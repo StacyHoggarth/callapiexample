0e1c3ae6a99630c355b708bf1342b62131064e56
Stacy Hoggarth <stacyhoggarth@stacys-imac.lan>
2020-07-31 14:55:56 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function App() {
+  const [books, setBooks] = useState(null);
+
+  const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+
+    const fetchData = async () => {
+        const response = await axios.get(apiURL)
+
+        setBooks(response.data) 
+    }
+
   return (
     <div className="App">
       <header className="App-header">
